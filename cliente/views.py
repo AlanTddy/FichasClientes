3b0fa5adcecde454857f81adf2402a0b5e24b6ba
AlanTddy <alan.almeyda9529@gmail.com>
2024-06-15 10:38:13 -0300

    pagar conta

--- cliente/views.py
+++ cliente/views.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Cliente, Compra, Historico
 from .forms import ClienteForm, CompraForm, HistoricoForm
@@ -53,18 +54,32 @@ def remove_compra(request, cliente_id, compra_id):
         return redirect('detalhe_cliente', cliente_id=cliente_id)
     return render(request, 'cliente/remover_compra.html', {'cliente': cliente, 'compra': compra})
 
-def remover_todas_compras(request, cliente_id):
+def remover_todas_compras(cliente_id):
     cliente = get_object_or_404(Cliente, id=cliente_id)
     compra = Compra.objects.filter(cliente=cliente)
-    if request.method == 'POST':
-        for compras in compra:
-            compras.delete()
-        return redirect('detalhe_cliente', cliente_id=cliente_id)
-    return render(request, 'cliente/pagar_conta.html', {'cliente': cliente, 'compra': compra})
+    for compras in compra:
+        compras.delete()
 
-def pagar_conta(request, cliente_id, compra_id, valorPago):
+def restante_conta(cliente, valor):
+    restante_conta = Compra(cliente=cliente, valor=valor)
+    restante_conta.save()
+    return restante_conta
+
+def pagar_conta(request, cliente_id):
     cliente = get_object_or_404(Cliente, id=cliente_id)
-    compras = get_object_or_404(Compra, id=compra_id)
+    compras = Compra.objects.filter(cliente = cliente)
+
     if request.method == 'POST':
-        total = sum(compras.valor for compra in compras)
-        total =- valorPago
+        valorPago = Decimal(request.POST.get('valorPago'))
+        total = sum(compra.valor for compra in compras)
+
+        if total > valorPago:
+            restante = total - valorPago
+            remover_todas_compras(cliente_id)
+            restante_conta(cliente, restante)
+        else:
+            remover_todas_compras(cliente_id)
+
+        return redirect('detalhe_cliente', cliente_id=cliente_id)
+    
+    return render(request, 'cliente/pagar_conta.html', {'cliente': cliente, 'compras': compras})     
